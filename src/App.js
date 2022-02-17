fb6fa773bb681b33901a365a4709aae29e0d88ed
stocke777 <deshwaljaivardhan@gmail.com>
2022-02-17 22:22:07 +0530

    just checking

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+      <div>{2+43}</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
