9793a2fcf84962f0dc38dd1519d2814655f00bef
stocke777 <deshwaljaivardhan@gmail.com>
2022-02-24 21:40:12 +0530

    random testing

---
+++ src/Potato.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Potato(props) {
+    console.log("nsaoicnnnnnnnnnnnnnnnnnnnn")
+    console.log(props)
+  return (
+    <div>Potato {props.value}</div>
+  )
+}
